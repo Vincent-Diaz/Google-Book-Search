434a019a894a0980fb6c63cfbc6e7010dbf2b40b
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-12-02 10:45:49 -0600

    added paths and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/NavBar/NavBar";
+import Header from "./components/Header/jumbotron";
+import Search from "./pages/search";
+import Saved from "./pages/saved";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <Nav />
+          <Header />
+          <Switch>
+            <Route exact path={["/", "/search"]}>
+              <Search />
+            </Route>
+            <Route exact path="/saved">
+              <Saved />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 

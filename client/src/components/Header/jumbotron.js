496f1e948ad3eaf9d2c6cccbba3ccceefe247f5d
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-12-02 10:47:06 -0600

    added header component with jumbotron

---
+++ client/src/components/Header/jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Jumbotron from 'react-bootstrap/Jumbotron'
+// import Container from 'react-bootstrap/Container'
+
+function Header() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Google Book Search</h1>
+            </div>
+        </div>
+    )
+}
+export default Header;

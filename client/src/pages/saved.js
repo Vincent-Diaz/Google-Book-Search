d0337605718384868e084d159ba68fe6f86ec155
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-12-03 19:42:54 -0600

    added saved page code with render

---
+++ client/src/pages/saved.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+
+class Saved extends Component {
+    state = {
+        savedBooks: []
+    };
+
+    componentDidMount() {
+        this.loadBooks();
+    }
+
+    deleteBook = id => {
+        API.deleteBook(id)
+            .then(res => this.loadbooks())
+            .catch(err => console.log(err));
+    }
+
+    loadBooks = () => {
+        API.savedBooks()
+            .then(({ data: savedBooks }) => {
+                this.setState({ savedBooks });
+            })
+            .catch(err => console.log(err));
+    }
+
+    render() {
+        return (
+            // <div className="container">
+            //     {!this.state.savedbooks.length ? (
+            //         <h1 className="text-center">No Results to Display</h1>
+            //     ) : (
+                        <div>
+                            {this.state.savedBooks.map(result => (
+                                <div className="card mb-3" key={result._id}>
+                                    <div className="row">
+                                        <div className="col-md-2">
+                                            <img alt={result.title} className="img-fluid" src={result.image} />
+                                        </div>
+                                        <div className="col-md-10">
+                                            <div className="card-body">
+                                                <h5 className="card-title">{result.title} by {result.author}</h5>
+                                                <p className="card-text">{result.description}</p>
+                                                <div>
+                                                    <a href={result.link} className="btn btn-outline-dark mt-3" target="_blank" rel="noreferrer" >View</a>
+                                                    <button onClick={() => this.deleteBook(result._id)} className="btn btn-outline-primary mt-3 ml-3" >
+                                                        Remove
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    // )}
+            // </div>
+        )
+    }
+}
+export default Saved;
